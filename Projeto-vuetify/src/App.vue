63e54dde5f8c3904256fd3a0e2a52ef84b229b35
tolentino15 <luiztm10@gmail.com>
2024-06-27 17:30:11 -0300

    Conceitos Basicos (NavDraw-AppBar-Grid)

--- Projeto-vuetify/src/App.vue
+++ Projeto-vuetify/src/App.vue
@@ -1,11 +1,46 @@
 <template>
-  <v-app>
+  <v-app >
+
+    <v-app-bar flat color="purple" class="border-b">
+
+      <v-app-bar-title>LUNIO</v-app-bar-title>
+
+    </v-app-bar>
+
+    <v-navigation-drawer permanent color="purple" width="flex" expand-on-hover rail >
+
+      <v-list-item
+       title="Dashboard"
+      prepend-avatar=".\assets\logo.png"
+
+       >
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list-group link title="Produtos"></v-list-group>
+      <v-divider></v-divider>
+      <v-list-item link title="Negocios"></v-list-item>
+      <v-divider></v-divider>
+      <v-list-item link title="Cadastro"></v-list-item>
+      <v-divider></v-divider>
+
+    </v-navigation-drawer>
     <v-main>
-      <HelloWorld />
+      
+        <v-container>
+          <h1>Dashboard</h1>
+          <v-row>
+            <v-col cols="4" > oi</v-col>
+            <v-col cols="4" > ola</v-col>
+            <v-col cols="4" >eae</v-col>
+          </v-row>
+        </v-container>
+
+        <HelloWorld></HelloWorld>
+    
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  //
+//
 </script>
--- Projeto-vuetify/src/plugins/vuetify.ts
+++ Projeto-vuetify/src/plugins/vuetify.ts
@@ -14,6 +14,9 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'light',
   },
+  icons: {
+    defaultSet: 'mdi', // This is already the default value - only for display purposes
+  }
 })
